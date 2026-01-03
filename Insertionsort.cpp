5565564830dc8aeed703a3d371e857b92a77b145
Krishna Asati <krishnaasati2006@gmail.com>
2026-01-03 15:22:01 +0530

    Insertion Sort

---
+++ Insertionsort.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int arr[], int n)
+{
+    for(int i = 0; i<=n-1;i++)
+   {
+         int j = i;
+         while(j>0 && arr[j-1] > arr[j])
+         {
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;
+            cout << "Runs";
+         }
+    } 
+}
+
+int main()
+{
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int choice;
+    cout << "\n1. Sort array\n2. Exit\nEnter option: ";
+    cin >> choice;
+
+    if(choice == 1)
+    {
+        insertion_sort(arr, n);
+        cout << "Sorted array: ";
+        for(int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+    }
+    else
+    {
+        cout << "Exit";
+    }
+
+    return 0;
+}
